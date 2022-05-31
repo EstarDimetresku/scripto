76c10710a795c4cf9778b4f91f17f9d95ad9a7e8
EstarDimetresku <84844187+EstarDimetresku@users.noreply.github.com>
2022-05-31 20:43:03 +0300

    Add files via upload

---
+++ scripto.bat
@@ -0,0 +1,22 @@
+echo off
+md Shemiakin
+cd Shemiakin
+md Artem
+md adb1908
+pause
+echo off
+echo > 11042000.txt
+cd adb1908
+echo > Afilla.txt
+cd..
+cd..
+pause
+echo off
+del Shemiakin /S /Q /F
+pause
+echo off
+cd Shemiakin
+rd Artem
+rd adb1908
+cd..
+rd Shemiakin
